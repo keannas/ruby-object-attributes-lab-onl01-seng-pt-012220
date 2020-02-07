50f9d6712df7c12bb4f642f9bdcd668d1cc997d0
Keanna Salter <keannasalter91@gmail.com>
2020-02-07 18:41:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,22 @@
 class Dog
   
-def name = (dogs_name)
-  @name = dogs_name
+  def name = (dogs_name)
+   @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed =(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
 
-def name
-  @name
-end
 
-def breed =(breed)
-  @breed = breed
-end
-
-
+  
   
